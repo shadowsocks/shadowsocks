32679bf0af97f27a6abd23ebccc87d113116d317
leo zhou <zhlwish@gmail.com>
2013-02-19 14:15:44 +0800

    Support python 2.5 for client

--- local.py
+++ local.py
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from __future__ import with_statement
 import sys
 
 try:
@@ -147,6 +148,12 @@ class Socks5Server(SocketServer.StreamRequestHandler):
 
 
 if __name__ == '__main__':
+    print "Python Version: %s " % '.'.join(str(v) for v in sys.version_info)
+    if sys.version_info < (2, 6):
+        import simplejson as json
+    else:
+        import json
+        
     os.chdir(os.path.dirname(__file__) or '.')
     print 'shadowsocks v0.9.3'
 
