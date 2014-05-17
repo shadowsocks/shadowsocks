ddbab8f9cd3294b40f2cc447b891d01eba19eef6
clowwindy <clowwindy42@gmail.com>
2014-05-17 15:00:12 +0800

    add test for workers

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import select
 import struct
 import hashlib
 import string
+import time
 from subprocess import Popen, PIPE
 
 target1 = [
@@ -105,6 +106,7 @@ try:
                 ready_count += 1
         
         if ready_count == 2 and p3 is None:
+            time.sleep(3)
             p3 = Popen(['curl', 'http://www.example.com/', '-v', '-L',
                         '--socks5-hostname', '127.0.0.1:1080'], shell=False,
                         bufsize=0,  close_fds=True)
