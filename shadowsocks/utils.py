cd22d474fe6744a8b1ef41902f4f6090cee9742b
clowwindy <clowwindy42@gmail.com>
2014-06-01 14:11:38 +0800

    almost done

--- shadowsocks/utils.py
+++ shadowsocks/utils.py
@@ -145,6 +145,7 @@ def get_config(is_local):
     logging.basicConfig(level=level,
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
+    return config
 
 
 def print_local_help():
