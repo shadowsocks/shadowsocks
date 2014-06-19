d949d11b281d81c0c47111537a0be649acfa471f
clowwindy <clowwindy42@gmail.com>
2014-06-19 10:01:55 +0800

    larger buffer

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setup(
     name="shadowsocks",
-    version="2.0.5",
+    version="2.0.6",
     license='MIT',
     description="A fast tunnel proxy that help you get through firewalls",
     author='clowwindy',
