1dd722acc7aab30e9ea7c48d6a502e6826dfd472
clowwindy <clowwindy42@gmail.com>
2014-06-21 11:23:44 +0800

    bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setup(
     name="shadowsocks",
-    version="2.0.6",
+    version="2.0.7",
     license='MIT',
     description="A fast tunnel proxy that help you get through firewalls",
     author='clowwindy',
