4699de1dd9ebc06adb16fb077e143d0571ad7ad6
clowwindy <clowwindy42@gmail.com>
2014-10-31 21:53:53 +0800

    fix python 3.2

--- shadowsocks/utils.py
+++ shadowsocks/utils.py
@@ -207,7 +207,7 @@ def get_config(is_local):
         level = logging.INFO
     logging.basicConfig(level=level,
                         format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
     check_config(config)
 
