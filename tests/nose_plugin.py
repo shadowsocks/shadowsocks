8454220adcb1ea47cb8a25e359e310513ee8913a
clowwindy <clowwindy42@gmail.com>
2014-10-31 15:02:12 +0800

    update unit test

---
+++ tests/nose_plugin.py
@@ -0,0 +1,27 @@
+import sys
+import nose
+from nose.plugins.base import Plugin
+
+class ExtensionPlugin(Plugin):
+
+    name = "ExtensionPlugin"
+
+    def options(self, parser, env):
+        Plugin.options(self,parser,env)
+
+    def configure(self, options, config):
+        Plugin.configure(self, options, config)
+        self.enabled = True
+
+    def wantFile(self, file):
+        return file.endswith('.py')
+
+    def wantDirectory(self,directory):
+        return True
+
+    def wantModule(self,file):
+        return True
+
+
+if __name__ == '__main__':
+    nose.main(addplugins=[ExtensionPlugin()])
