9d33a59b609d04c129396dba4ddb56d2cee07f97
clowwindy <clowwindy42@gmail.com>
2014-12-10 23:15:24 +0800

    add libsodium tests

---
+++ tests/libsodium/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -d libsodium-1.0.1 ]; then
+    wget https://github.com/jedisct1/libsodium/releases/download/1.0.1/libsodium-1.0.1.tar.gz || exit 1
+    tar xf libsodium-1.0.1.tar.gz || exit 1
+fi
+pushd libsodium-1.0.1
+./configure && make && make install || exit 1
+popd
