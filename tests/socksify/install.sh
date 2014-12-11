c214dbf7b593b130ad5fd3f970e2ccd5e73b0a24
clowwindy <clowwindy42@gmail.com>
2014-12-11 12:46:52 +0800

    parallel libsodium and socksify builds

--- tests/socksify/install.sh
+++ tests/socksify/install.sh
@@ -5,6 +5,6 @@ if [ ! -d dante-1.4.0 ]; then
     tar xf dante-1.4.0.tar.gz || exit 1
 fi
 pushd dante-1.4.0
-./configure && make && make install || exit 1
+./configure && make -j4 && make install || exit 1
 popd
 cp tests/socksify/socks.conf /etc/ || exit 1
