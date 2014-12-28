45ee594d587fa6606043ed08638405d2910fc0ae
clowwindy <clowwindy42@gmail.com>
2014-12-28 12:29:20 +0800

    coverage server now supports more projects

--- tests/coverage_server.py
+++ tests/coverage_server.py
@@ -6,16 +6,19 @@ if __name__ == '__main__':
     import urllib
 
     class MainHandler(tornado.web.RequestHandler):
-        def get(self):
-            with open('/tmp/shadowsocks-coverage', 'rb') as f:
-                coverage = f.read().strip()
-                self.redirect(('https://img.shields.io/badge/'
-                               'coverage-%s-brightgreen.svg'
-                               '?style=flat') %
-                              urllib.quote(coverage))
+        def get(self, project):
+            try:
+                with open('/tmp/%s-coverage' % project, 'rb') as f:
+                    coverage = f.read().strip()
+                    self.redirect(('https://img.shields.io/badge/'
+                                'coverage-%s-brightgreen.svg'
+                                '?style=flat') %
+                                urllib.quote(coverage))
+            except IOError:
+                raise tornado.web.HTTPError(404)
 
     application = tornado.web.Application([
-        (r"/shadowsocks", MainHandler),
+        (r"/([a-zA-Z0-9\\-_]+)", MainHandler),
     ])
 
     if __name__ == "__main__":
