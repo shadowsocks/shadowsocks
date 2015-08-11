a2bc6e19457f51a421b7d2866be5903e0d71fd2f
clowwindy <clowwindy42@gmail.com>
2015-08-11 17:55:16 +0800

    fix #414

--- shadowsocks/server.py
+++ shadowsocks/server.py
@@ -42,12 +42,13 @@ def main():
                          'will be ignored')
     else:
         config['port_password'] = {}
-        server_port = config['server_port']
-        if type(server_port) == list:
-            for a_server_port in server_port:
-                config['port_password'][a_server_port] = config['password']
-        else:
-            config['port_password'][str(server_port)] = config['password']
+        server_port = config.get('server_port', None)
+        if server_port:
+            if type(server_port) == list:
+                for a_server_port in server_port:
+                    config['port_password'][a_server_port] = config['password']
+            else:
+                config['port_password'][str(server_port)] = config['password']
 
     if config.get('manager_address', 0):
         logging.info('entering manager mode')
