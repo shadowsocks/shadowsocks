b6961974ba39a912cd685c11264e9a1611a73c47
falseen <hurxyill@163.com>
2015-09-11 20:23:27 +0800

    兼容py3，修正语法错误，加载libcrypto模块失败时显示详细错误。
    修正语法错误：` except Exception, e: ` 是旧版本的语法，py2.6之后的版本已不再使用此语法。
    加载libcrypto模块失败时显示详细错误。

--- shadowsocks/crypto/util.py
+++ shadowsocks/crypto/util.py
@@ -88,7 +88,7 @@ def find_library(possible_lib_names, search_symbol, library_name):
                 logging.warn('can\'t find symbol %s in %s', search_symbol,
                              path)
         except ValueError as e:
-            print(e)
+            logging.error(e)
     return None
 
 
