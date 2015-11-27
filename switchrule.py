4894925cacb52edb8a5e6a5b0f863a97a4a6207a
BreakWa11 <mmgac001@gmail.com>
2015-11-27 16:47:08 +0800

    add demo code in switchrule.py

--- switchrule.py
+++ switchrule.py
@@ -1,6 +1,8 @@
 def getKeys():
 	return ['port', 'u', 'd', 'transfer_enable', 'passwd', 'enable' ]
+	#return ['port', 'u', 'd', 'transfer_enable', 'passwd', 'enable', 'plan' ] # append the column name 'plan'
 
 def isTurnOn(row):
 	return True
+	#return row['plan'] == 'B' # then judge here
 
