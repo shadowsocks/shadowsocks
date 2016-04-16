1f24b31f891e237cf17949dfc42e280bc077c8cc
mengskysama <mengskysama@gmail.com>
2016-04-16 11:07:22 +0800

    Update install.sh
    replace dante corrupt src

--- tests/socksify/install.sh
+++ tests/socksify/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 if [ ! -d dante-1.4.0 ]; then
-    wget http://www.inet.no/dante/files/dante-1.4.0.tar.gz || exit 1
+    #wget http://www.inet.no/dante/files/dante-1.4.0.tar.gz || exit 1
+    wget https://codeload.github.com/notpeter/dante/tar.gz/dante-1.4.0 -O dante-1.4.0.tar.gz || exit 1
     tar xf dante-1.4.0.tar.gz || exit 1
+    #
+    mv dante-dante-1.4.0 dante-1.4.0
 fi
 pushd dante-1.4.0
 ./configure && make -j4 && make install || exit 1
