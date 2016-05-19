cb866dc8fe524d15264c2359f743fcb1e8d4d2f2
BreakWa11 <mmgac001@gmail.com>
2016-05-19 13:42:52 +0800

    set udp_timeout default value

--- shadowsocks/shell.py
+++ shadowsocks/shell.py
@@ -236,7 +236,7 @@ def get_config(is_local):
     config['obfs_param'] = to_str(config.get('obfs_param', ''))
     config['port_password'] = config.get('port_password', None)
     config['timeout'] = int(config.get('timeout', 300))
-    config['udp_timeout'] = int(config.get('udp_timeout', config['timeout']))
+    config['udp_timeout'] = int(config.get('udp_timeout', 120))
     config['udp_cache'] = int(config.get('udp_cache', 64))
     config['fast_open'] = config.get('fast_open', False)
     config['workers'] = config.get('workers', 1)
