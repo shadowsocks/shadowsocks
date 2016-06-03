6932548d8e642450f04822b25b42c7168080921e
BreakWa11 <mmgac001@gmail.com>
2016-06-03 21:18:50 +0800

    ulimit -n 512000

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
 eval $(ps -ef | grep "[0-9] python server\\.py m" | awk '{print "kill "$2}')
-ulimit -n 131072
+ulimit -n 512000
 nohup python server.py m>> ssserver.log 2>&1 &
 
