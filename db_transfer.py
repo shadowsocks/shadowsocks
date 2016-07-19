794121f11171373a05b7291eaf1a86be7537b772
BreakWa11 <mmgac001@gmail.com>
2016-07-19 21:29:07 +0800

    show port details

--- db_transfer.py
+++ db_transfer.py
@@ -205,7 +205,9 @@ class DbTransfer(object):
 
 			elif allow and ServerPool.get_instance().server_run_status(port) is False:
 				#new_servers[port] = passwd
-				logging.info('db start server at port [%s] pass [%s]' % (port, passwd))
+				protocol = cfg.get('protocol', ServerPool.get_instance().config.get('protocol', 'origin'))
+				obfs = cfg.get('obfs', ServerPool.get_instance().config.get('obfs', 'plain'))
+				logging.info('db start server at port [%s] pass [%s] protocol [%s] obfs [%s]' % (port, passwd, protocol, obfs))
 				ServerPool.get_instance().new_server(port, cfg)
 
 		for row in last_rows:
@@ -222,7 +224,9 @@ class DbTransfer(object):
 			self.event.wait(eventloop.TIMEOUT_PRECISION + eventloop.TIMEOUT_PRECISION / 2)
 			for port in new_servers.keys():
 				passwd, cfg = new_servers[port]
-				logging.info('db start server at port [%s] pass [%s]' % (port, passwd))
+				protocol = cfb.get('protocol', ServerPool.get_instance().config.get('protocol', 'origin'))
+				obfs = cfb.get('obfs', ServerPool.get_instance().config.get('obfs', 'plain'))
+				logging.info('db start server at port [%s] pass [%s] protocol [%s] obfs [%s]' % (port, passwd, protocol, obfs))
 				ServerPool.get_instance().new_server(port, cfg)
 
 	@staticmethod
