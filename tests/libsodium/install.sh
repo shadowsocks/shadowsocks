0feb8b2eb3983f05b7b1b8ae0eb7e6bbd16c557d
smounives <smounives@outlook.com>
2016-08-17 05:06:59 +0000

    Add chacha20-ietf crypto

--- tests/libsodium/install.sh
+++ tests/libsodium/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -d libsodium-1.0.1 ]; then
-    wget https://github.com/jedisct1/libsodium/releases/download/1.0.1/libsodium-1.0.1.tar.gz || exit 1
-    tar xf libsodium-1.0.1.tar.gz || exit 1
+if [ ! -d libsodium-1.0.11 ]; then
+    wget https://github.com/jedisct1/libsodium/releases/download/1.0.11/libsodium-1.0.11.tar.gz || exit 1
+    tar xf libsodium-1.0.11.tar.gz || exit 1
 fi
-pushd libsodium-1.0.1
+pushd libsodium-1.0.11
 ./configure && make -j2 && make install || exit 1
 sudo ldconfig
 popd
