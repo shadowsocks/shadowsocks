e2957e672e7d50e297535126c02ae181ebba966d
破娃酱 <mmgac001@gmail.com>
2017-02-14 23:28:49 +0800

    timeout set to 1

--- shadowsocks/tcprelay.py
+++ shadowsocks/tcprelay.py
@@ -97,7 +97,7 @@ UDP_MAX_BUF_SIZE = 65536
 class SpeedTester(object):
     def __init__(self, max_speed = 0):
         self.max_speed = max_speed * 1024
-        self.timeout = 5
+        self.timeout = 1
         self._cache = deque()
         self.sum_len = 0
 
