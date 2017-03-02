6db9ebda49502b762c76e6e1b186ea409d6ddf86
Zou Yong <zouy02@gmail.com>
2017-03-02 18:46:06 +0800

    remove unused import

--- shadowsocks/server.py
+++ shadowsocks/server.py
@@ -22,7 +22,6 @@ import sys
 import os
 import logging
 import signal
-import multiprocessing
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 from shadowsocks import shell, daemon, eventloop, tcprelay, udprelay, \
