6c162b13c417d5bf1f18f989a247c613062b54a2
破娃酱 <mmgac001@gmail.com>
2017-05-12 17:13:58 +0800

    fix stop script

--- shadowsocks/stop.sh
+++ shadowsocks/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python_ver=$(ls /usr/bin|grep -e "^python[23]\.[1-9]\+$"|tail -1)
-eval $(ps -ef | grep "[0-9]  ${python_ver} server\\.py a" | awk '{print "kill "$2}')
+eval $(ps -ef | grep "[0-9] ${python_ver} server\\.py a" | awk '{print "kill "$2}')
 
