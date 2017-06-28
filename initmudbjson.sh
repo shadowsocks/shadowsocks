bc930031a9e6d429e23cc8e5801d8206312cc043
破娃酱 <mmgac001@gmail.com>
2017-06-28 16:47:05 +0800

    update initmudbjson.sh

--- initmudbjson.sh
+++ initmudbjson.sh
@@ -6,7 +6,7 @@ ip_addr=`ifconfig -a|grep inet|grep -v inet6|grep -v "127.0.0."|grep -v -e "192\
 ip_count=`echo $ip_addr|grep -e "^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$" -c`
 if [[ $ip_count == 1 ]]; then
 	echo "server IP is "${ip_addr}
-	sed -i 's/SERVER_PUB_ADDR = .\+/SERVER_PUB_ADDR = '${ip_addr}'/g' userapiconfig.py
+	sed -i "s/SERVER_PUB_ADDR = .\+/SERVER_PUB_ADDR = \'"${ip_addr}"\'/g" userapiconfig.py
 	user_count=`python mujson_mgr.py -l|grep -c -e "[0-9]"`
 	if [[ $user_count == 0 ]]; then
 		port=`python -c 'import random;print(random.randint(10000, 65536))'`
